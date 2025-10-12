e39390c4b8f18fc4d1e278489d61bb0364666343
Mahmud Hasan <mahmud.buet.me@gmail.com>
2025-10-11 22:26:50 -0600

    added bubble sort

--- colt_steele/sorting_algos/all_algos.py
+++ colt_steele/sorting_algos/all_algos.py
@@ -1,20 +1,41 @@
-def bubble_sort(arr):
-    def swap(arr, idx1, idx2):
-        arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+def swap(arr, idx1, idx2):
+    """
+    :param arr: the array/list supplied
+    :param idx1: element at idx1 to be swapped to idx2
+    :param idx2: element at idx2 to be swapped to idx1
+    :return: no need to return, list mutated inplace
+    """
+    arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
 
-    print(arr)
-    for i in range(len(arr) - 1, -1, -1):
-        # lets increment j from 0 upto 1 step before ith position
-        # because j+1 will be the ith position
-        for j in range(0, i, 1):
-            print(i, j)
-            print(f"comparing : {arr[j]} - {arr[j + 1]}")
+
+def bubble_sort(arr):
+    # let start i from len(arr) which is 1 + max possible index
+    # doing this so that j can go from 0 to i-2 inclusive
+    # so j + 1 will be 1 to i-1 inclusive
+    # we want the i-th position to be blocked off after every pass as it will be sorted
+    called_swap = False
+    for i in range(len(arr), -1, -1):
+        for j in range(0, i - 1, 1):
             if arr[j] > arr[j + 1]:
                 swap(arr, j, j + 1)
-                print("swap called")
-            print(f"[INFO] array after pass: {arr}......")
-            print("=" * 50)
+                called_swap = True
+        print(f"[INFO] completed 1 pass......")
+        print(f"i : {i}, j : {j}")
+        print(arr)
+        print("=" * 50)
+
+        if called_swap:
+            print(f"[INFO] swap called in last pass......")
+            called_swap = not called_swap
+        else:
+            # we did not call swap
+            print(f"[INFO] swap NOT called in last pass......")
+            break
 
 
 if __name__ == '__main__':
-    print(bubble_sort([29, 10, 9, 5, 37, 18]))
+    print(bubble_sort([42, 7, 91, 3, 56, 12, 88, 25, 1, 67,
+                       34, 73, 8, 54, 29, 15, 61, 2, 48, 90,
+                       19, 33, 77, 5, 81, 26, 63, 14, 39, 11,
+                       94, 23, 70, 6, 45, 32, 80, 27, 17, 99,
+                       9, 68, 13, 58, 47, 36, 4, 65, 18, 53]))
