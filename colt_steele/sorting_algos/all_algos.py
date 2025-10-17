1d88d15d09806f7406434db5dac6c1ead7e7fb6e
Mahmud Hasan <mahmud.buet.me@gmail.com>
2025-10-17 00:02:59 -0600

    update commit

--- colt_steele/sorting_algos/all_algos.py
+++ colt_steele/sorting_algos/all_algos.py
@@ -33,8 +33,38 @@ def bubble_sort(arr):
             break
 
 
+def selection_sort(arr: list):
+    print(f"starting with: {arr}")
+
+    for i in range(0, len(arr), 1):
+
+        for j in range(i, len(arr), 1):
+            if j == 0:
+                current_min_index = j
+            if arr[current_min_index] > arr[j]:
+                current_min_index = j
+            elif arr[current_min_index] < arr[j]:
+                current_min_index = current_min_index
+            else:
+                pass
+        # after 1 pass swap
+        print("one pass completed", i, j, current_min_index)
+        if i != current_min_index:
+            print("calling swap")
+            swap(arr,idx1=i, idx2=current_min_index)
+            print(arr)
+            print("=" * 50)
+
+
 if __name__ == '__main__':
-    print(bubble_sort([42, 7, 91, 3, 56, 12, 88, 25, 1, 67,
+    # print(bubble_sort([42, 7, 91, 3, 56, 12, 88, 25, 1, 67,
+    #                    34, 73, 8, 54, 29, 15, 61, 2, 48, 90,
+    #                    19, 33, 77, 5, 81, 26, 63, 14, 39, 11,
+    #                    94, 23, 70, 6, 45, 32, 80, 27, 17, 99,
+    #                    9, 68, 13, 58, 47, 36, 4, 65, 18, 53]))
+    #print(selection_sort([5, 3, 4, 1, 2, 0, 9, 3, 2, 1]))
+
+    print(selection_sort([42, 7, 91, 3, 56, 12, 88, 25, 1, 67,
                        34, 73, 8, 54, 29, 15, 61, 2, 48, 90,
                        19, 33, 77, 5, 81, 26, 63, 14, 39, 11,
                        94, 23, 70, 6, 45, 32, 80, 27, 17, 99,
